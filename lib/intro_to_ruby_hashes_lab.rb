bd741a777b5884a167fac09ec8c6dd508876f748
James Sapp <jameseisapp@gmail.com>
2020-11-10 00:17:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,7 @@ end
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
   hash[key]
-  return hash
+  return hash 
 end
 
 def update_counting_hash(hash, key)
